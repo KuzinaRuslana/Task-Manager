ddde101d58eb5c354d0fc858f8c22c1d7b2db817
Ruslana <kuzina.ruslana@yandex.ru>
2025-03-12 20:51:35 +0300

    Fix main page and styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,43 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ru">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-param" content="_token" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Менеджер задач</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+            <header class="fixed w-full">
+                <nav>
+                    @include('layouts.navigation')
+                </nav>
+            </header>
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                <section class="bg-white dark:bg-gray-900">
+                    <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+                        <div class="grid col-span-full">
+
+                            @include('flash::message')
+
+                            @yield('content')
+    
+                        </div>
+                    </div>
+                </section> 
             </main>
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskStatusController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,10 +12,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::resource('task_statuses', TaskStatusController::class)
+    ->only(['store', 'update', 'destroy'])
+    ->middleware('auth');
+
+Route::resource('task_statuses', TaskStatusController::class)
+    ->except(['create', 'edit', 'store', 'update', 'destroy']);
 
 require __DIR__.'/auth.php';
