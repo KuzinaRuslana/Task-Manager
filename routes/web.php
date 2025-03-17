761806a5f89eafe53335aacceddde3d0d2e47452
Ruslana <kuzina.ruslana@yandex.ru>
2025-03-17 21:57:45 +0300

    Fix routing bugs

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,22 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('task_statuses', TaskStatusController::class)
-    ->only(['index', 'show']);
+    ->only(['index']);
 
 Route::middleware('auth')->group(function () {
     Route::resource('task_statuses', TaskStatusController::class)
-    ->only(['create', 'store', 'edit', 'update', 'destroy']);
+        ->only(['create', 'store', 'edit', 'update', 'destroy']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 });
 
 Route::resource('tasks', TaskController::class)
     ->only(['index', 'show']);
-
-Route::resource('tasks', TaskController::class)
-    ->only(['create', 'store', 'edit', 'update', 'destroy'])
-    ->middleware('auth');
 
 require __DIR__.'/auth.php';
