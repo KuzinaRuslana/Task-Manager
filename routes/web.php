424985588b60de81ff6ae9dbd132d4f8aabb6ae1
Ruslana <kuzina.ruslana@yandex.ru>
2025-03-17 22:05:51 +0300

    Optimize the routes

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,9 @@ Route::resource('task_statuses', TaskStatusController::class)
 Route::middleware('auth')->group(function () {
     Route::resource('task_statuses', TaskStatusController::class)
         ->only(['create', 'store', 'edit', 'update', 'destroy']);
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-    Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+    
+    Route::resource('tasks', TaskController::class)
+        ->only(['create', 'store', 'edit', 'update', 'destroy']);
 });
 
 Route::resource('tasks', TaskController::class)
