2c6a1b163e3544d0cfb5edfb647f7609392816a0
Ruslana <kuzina.ruslana@yandex.ru>
2025-03-20 20:06:51 +0300

    Fix rendering #2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Vite::useSecureAssetUrls();
     }
 }
