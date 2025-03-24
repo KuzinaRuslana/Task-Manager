fbacac55f71517f15861fae74f3fd20baa61ec46
Ruslana <kuzina.ruslana@yandex.ru>
2025-03-24 20:48:52 +0300

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,24 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('tasks', TaskController::class)->only(['index', 'show']);
-Route::resource('labels', LabelController::class)->only(['index']);
-Route::resource('task_statuses', TaskStatusController::class)->only(['index']);
+Route::resource('task_statuses', TaskStatusController::class)
+    ->only(['index']);
 
 Route::middleware('auth')->group(function () {
-    Route::resource('tasks', TaskController::class)->except(['index', 'show']);
-    Route::resource('labels', LabelController::class)->except(['index']);
-    Route::resource('task_statuses', TaskStatusController::class)->except(['index']);
+    Route::resource('task_statuses', TaskStatusController::class)
+        ->only(['create', 'store', 'edit', 'update', 'destroy']);
+    
+    Route::resource('tasks', TaskController::class)
+        ->only(['create', 'store', 'edit', 'update', 'destroy']);
+
+    Route::resource('labels', LabelController::class)
+        ->only(['create', 'store', 'edit', 'update', 'destroy']);
 });
+
+Route::resource('tasks', TaskController::class)
+    ->only(['index', 'show']);
+
+Route::resource('labels', LabelController::class)
+    ->only(['index']);
 
 require __DIR__.'/auth.php';
