241cfc2be3bca0540e02e78cd22a98fff09ea02c
Ruslana <kuzina.ruslana@yandex.ru>
2025-03-27 19:11:33 +0300

    Fix validation messages

---
+++ resources/lang/ru/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'required' => 'Поле :attribute обязательно.',
+    'email' => 'Введите корректный email.',
+    'min' => [
+        'string' => 'Пароль должен иметь длину не менее 8 символов',
+    ],
+    'confirmed' => 'Пароль и подтверждение не совпадают',
+
+    'attributes' => [
+        'email' => 'Email',
+        'password' => 'Пароль',
+        'name' => 'Имя',
+        'password_confirmation' => 'Подтверждение пароля',
+    ],
+];
